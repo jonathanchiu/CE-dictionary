bb25ab9aa419d2fd9cb96f031d3987db12e95a9b
Jon Chiu <jonathanchiu7@gmail.com>
2016-05-30 01:51:36 -0400

    Add update and text to speech functionality * Pretty up design * Hook up Bootstrap Table and jQuery Tabledit plugins properly

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
  */
 app.get('/search/:type/:query', function(req, res) {
   var sqlQuery = "SELECT * FROM dictionary WHERE " + req.params.type + " REGEXP '[[:<:]]" + req.params.query + "[[:>:]]'";
-  
+
   connection.query(sqlQuery, function(err, rows, fields) {
     if (err) {
       throw err;
@@ -37,7 +37,30 @@ app.get('/search/:type/:query', function(req, res) {
 
 });
 
-app.post('/testing', function(req, res) {
+app.post('/update-delete', function(req, res) {
+  if (req.body.action === 'edit') {
+    var id = req.body.id;
+    var traditional = req.body.traditional;
+    var simplified = req.body.simplified;
+    var pinyin_numbers = req.body.pinyin_numbers;
+    var pinyin_marks = req.body.pinyin_marks;
+    var translation = req.body.translation;
+
+    var sqlQuery = `UPDATE dictionary SET traditional='${traditional}', simplified='${simplified}',
+      pinyin_numbers='${pinyin_numbers}', pinyin_marks='${pinyin_marks}', translation='${translation}' WHERE id=${id}`;
+
+    console.log(sqlQuery);
+
+    connection.query(sqlQuery, function(err, rows, fields) {
+      if (err) {
+        throw err;
+      }
+      res.end('{"success" : "Updated Successfully", "status" : 200}');
+    });
+  } 
+  else if (req.body.action === 'delete') {
+
+  }
   console.log(req.params);
   console.log(req.query.id);
   console.log(req.body);
--- public/dictionary.js
+++ public/dictionary.js
@@ -1,13 +1,43 @@
 $(function() {
 
+  function initChineseSpeech() {
+    $(".play-audio").on('click', function() {
+      var textToRead = $(this).closest('tr').find("td:eq(2) input").val();
+      responsiveVoice.speak(textToRead, 'Chinese Female');
+    }); 
+  }
+
+  function initEditableTable() {
+    $("#search-results").Tabledit({
+      url: '/update-delete',
+      columns: {
+        identifier: [0, 'id'],
+        editable: [[1, 'pinyin_marks'], [2, 'pinyin_numbers'], [3, 'simplified'],
+                  [4, 'traditional'], [5, 'translation']]
+      },
+      onAjax: function(action, serialize) {
+          console.log('onAjax(action, serialize)');
+          console.log(action);
+          console.log(serialize);
+      }
+    });
+  }
+
   $('#search-btn').click(function() {
+    var playAudio = "<button type='button' class='btn btn-sm btn-default play-audio' style='float: none;'>" + 
+          "<span class='glyphicon glyphicon-play'></span></button>";
     var searchType = $("form input[type='radio']:checked").val();
     var searchQuery = $("#search-field").val();
+
+    // Must destroy the old table before rendering new one with new data
+    $("#search-results").bootstrapTable('destroy');
 
     $("#search-results").bootstrapTable({
       method: 'GET',
       url: '/search/' + searchType + '/' + searchQuery,
       pagination: true,
+      search: true,
+      sortable: true,
       columns: [
       {
           field: 'id',
@@ -34,20 +64,19 @@ $(function() {
           title: 'Definition'
       }
       ],
-      onLoadSuccess: function (data) {
-        $("#search-results").Tabledit({
-          url: '/testing',
-          columns: {
-            identifier: [0, 'id'],
-            editable: [[1, 'traditional'], [2, 'simplified'], [3, 'pinyin_numbers'],
-                      [4, 'pinyin_marks'], [5, 'translation']]
-          },
-          onAjax: function(action, serialize) {
-              console.log('onAjax(action, serialize)');
-              console.log(action);
-              console.log(serialize);
-          }
-        });
+      onLoadSuccess: function() {
+        // Initialize the table to be editable (update/delete)
+        initEditableTable();
+        $(".btn-group-sm").append(playAudio);
+        // Initialize the text to speech click event once data has loaded
+        initChineseSpeech();
+      },
+      onPageChange: function() {
+        // These need to be reinitialized on a page change because it causes
+        // the DOM to change and the click events refer to old DOM elements
+        initEditableTable();
+        $(".btn-group-sm").append(playAudio);
+        initChineseSpeech();
       }
     });
   });
